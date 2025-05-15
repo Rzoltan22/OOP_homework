45a998f9eb68dfa05ecbf583dd1714e9b7ab4740
Rzoltan22 <raczzoltan38@gmail.com>
2025-05-15 11:31:37 +0200

    wtf

--- Documents/Vscode/Hazifeladat.py
+++ Documents/Vscode/Hazifeladat.py
@@ -1,3 +1,10 @@
-print("Hello")
-print("Hello")
-print("Hello")
+# Absztrakt osztály: Járat
+class Jarat(ABC):
+    def __init__(self, jaratszam, honnanhova, jegyar):
+        self.jaratszam = jaratszam
+        self.honnanhova = honnanhova
+        self.jegyar = jegyar
+
+    @abstractmethod
+    def jarat_tipus(self):
+        pass
